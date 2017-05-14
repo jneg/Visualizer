694200c74eae5e64811115e420237776046b9ff4
Javon Negahban <javon.negahban@cox.net>
2017-05-14 18:50:05 +0000

    Removed unnecessary functions from ScriptHistory DAO

--- Dao/ScriptHistory.js
+++ Dao/ScriptHistory.js
@@ -1,10 +1,6 @@
 const Conn = require('./Conn');
 const DaoHelper = require('./DaoHelper');
 
-const scriptGetSid = (sid) => `select * from Script where sid = ${sid}`;
-
-const scriptGetName = (name) => `select * from Script where name = "${name}"`;
-
 const scriptHistoryAll = (sid) => `select srs.hid, srs.start_time, srs.end_time, srs.status, ts.inserted, ts.modified, ts.deleted, srs.notes from ScriptRunSummary srs left join TableStatus ts on srs.hid = ts.hid join Script s on srs.sid = s.sid where srs.sid = ${sid} order by srs.hid desc limit 50`
 
 exports.getScriptHistory = (sid, cb) => {
